90502d07e8bd8790243aebcf168f01d93b5dd258
yuanyu <yuanyu90221@gmail.com>
2020-02-12 15:11:59 +0800

    update

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 func main() {
 	fmt.Println("Starting hello-world server...")
 	http.HandleFunc("/", helloServer)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":8088", nil); err != nil {
 		panic(err)
 	}
 }
 
 func helloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello world!")
+	fmt.Fprint(w, "Hello world From Okteto!")
 }
